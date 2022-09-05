9b0d53d10c2ac89596985c703647a823b7331a13
xibei <792823599@qq.com>
2022-09-05 18:24:29 +0800

    master1

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -6,5 +6,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println(" Hello");
         System.out.println("hot-fix1");
+        System.out.println("master1");
     }
 }
