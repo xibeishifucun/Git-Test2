cac19b47c762e17901cff9dde00942d3288e10ec
xibei <792823599@qq.com>
2022-09-05 21:09:26 +0800

    push test2

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -9,5 +9,7 @@ public class Test1 {
         System.out.println("master1");
         System.out.println("hot-fix2");
         System.out.println("hot-fix2-1");
+        System.out.println("push test");
+        System.out.println("push test2");
     }
 }
