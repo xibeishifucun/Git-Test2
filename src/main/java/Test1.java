1e90e20e6e3995abe72daac6c9dec4939fa44d4b
xibei <792823599@qq.com>
2022-09-05 21:27:17 +0800

    push test5

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -13,5 +13,6 @@ public class Test1 {
         System.out.println("push test2");
         System.out.println("push test3");
         System.out.println("push test4");
+        System.out.println("push test5");
     }
 }
